a1c8734f8b8c12fcfa48345d092794fed47a0a94
Michael Smith <smithms91@gmail.com>
2018-05-14 20:59:57 -0400

    First commit of Ghetto Reddit

---
+++ script.js
@@ -0,0 +1,32 @@
+$(document).ready(() => {
+  $.get("https://www.reddit.com/r/mildlyinteresting/.json").then((data) => {
+    for (let i = 0; i < 10; i++) {
+      let info = data.data.children[i].data.title;
+      let image = data.data.children[i].data.url;
+      let thumbnail = data.data.children[i].data.thumbnail;
+      let subreddit = data.data.children[i].data.subreddit_name_prefixed;
+      let author = data.data.children[i].data.author;
+      let linkToSite = data.data.children[i].data.permalink;
+      let bigImage = data.data.children[i].data.preview.images["0"].source.url;
+  
+      console.log(bigImage);
+      $("#container_box").append(`
+      <div class="container">
+        <div class="image">
+          <img src="${bigImage}" class="img">
+          <a href="${image}"><i class="material-icons plus_sign">zoom_in</i></a>
+          <div class="sub_reddit">${subreddit}</div>
+        </div>
+        <div class="content">
+          <h4 class="title">${info}</h4>
+          <div class="author">
+            <i class="material-icons author_icon">person</i>
+            <p class="person">${author}</p>
+            <a href="https://www.reddit.com${linkToSite}" class="view_site"><p>View Comments</p></a>
+          </div>
+        </div>
+      </div>
+      `);
+    }
+  })
+})
